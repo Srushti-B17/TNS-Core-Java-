848250af6c51bac8c22bf3aa03d20701f851eaad
Srushti Bhadane <131882017+Srushti-B17@users.noreply.github.com>
2023-10-31 16:10:03 +0530

    Add files via upload

---
+++ src/Datatype_Demo.java
@@ -0,0 +1,23 @@
+
+public class Datatype_Demo {
+
+	public static void main(String[] args)
+	{
+		
+		int number1=57;
+		int val = 30*4;
+		float $val3= 45/8f;
+		long val4=897653428265l;
+		
+		System.out.println(number1);
+		System.out.println(val);
+		System.out.println($val3);
+		System.out.println(val4);
+		
+		System.out.println("here is addition of varibles");
+		
+		double addition =number1+val+$val3+val4;
+		System.out.println(addition);
+		
+	}
+}
